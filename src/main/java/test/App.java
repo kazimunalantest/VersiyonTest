6d645538c6d772c2ac15fc71772bcd79c6ee8ba8
Kazım Ünalan <kazim.unalan@mebitech.com>
2018-03-12 15:10:08 +0300

    fist commit

---
+++ src/main/java/test/App.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello wrorld 2");
+        System.out.println("Hello wrorld 2");
+        System.out.println("Hello wrorld 2");
+    }
+}
